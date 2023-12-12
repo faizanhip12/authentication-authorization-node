68e32955c3b5e765c140d152583d3245432d3930
faizanhip12 <113345962+faizanhip12@users.noreply.github.com>
2023-12-12 21:55:24 +0500

    changesBackend

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,47 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { config } from "dotenv";
+config()
+
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
+
+
+app.use(express.static(__dirname + '/public/'));
+app.use("/upload", express.static("src/upload"));
+
+// app.use("/api/v1", [
+
+// ]);
+
+
+
+app.use((err: any, res: any) => {
+  console.log("err.message",err.message )
+  res.status(500).send({ message: err.message || "Network Error" })
+})
+
+
+export default app;
